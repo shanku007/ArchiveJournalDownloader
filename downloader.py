89985ea76e588e82588cd82d05ebd2247d8e0e25
Shankar Jha <shankar.jha@avizva.com>
2021-05-28 02:16:54 +0530

    The non threading version

--- downloader.py
+++ downloader.py
@@ -5,17 +5,7 @@ from util import read_txt_file
 from urllib.parse import urlsplit
 from constants import *
 import logging
-import threading
 
-
-def download_concurrently(each_link,logger,save_dir):
-    logger.info(f"Downloading from : {each_link}")
-    link_attr = urlsplit(each_link)
-    if link_attr.netloc == ARCHIVE:
-        down_inst = ArchiveDownloader(each_link,logger,save_dir)
-    elif link_attr.netloc == EAP_BL_UK:
-        down_inst = EapDownloader(each_link,logger,save_dir)
-    down_inst.download()
 
 class Downloader:
 
@@ -26,16 +16,18 @@ class Downloader:
 
     def download(self):
         links = read_txt_file(self.path)
-        threads = []
+
         for each_link in links:
-            logger = self.logger
-            save_dir = self.save_dir
-            thread = threading.Thread(target=download_concurrently, args=(each_link,logger,save_dir))
-            threads.append(thread)
-            thread.start()
-        for index,thread in enumerate(threads):
-            thread.join()
-            
+            self.logger.info(f"Downloading from : {each_link}")
+            link_attr = urlsplit(each_link)
+            if link_attr.netloc == ARCHIVE:
+                down_inst = ArchiveDownloader(each_link,self.logger,self.save_dir)
+            elif link_attr.netloc == EAP_BL_UK:
+                down_inst = EapDownloader(each_link,self.logger,self.save_dir)
+            down_inst.download()
+
+
+        
 
 if __name__ == "__main__":
     dow = Downloader("links.txt","Journal",logging)
